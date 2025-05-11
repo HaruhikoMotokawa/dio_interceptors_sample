90931e4ea70992267ceed3fa97934cc5dcd3d48c
本川　晴彦 <harx6009@icloud.com>
2025-05-11 14:12:37 +0900

    feat: DioCacheInterceptorとDioSmartRetryを追加し、HTTPリクエストのキャッシュ機能を実装

---
+++ lib/data/sources/http/interceptors/custom_dio_cache_interceptor.dart
@@ -0,0 +1,41 @@
+import 'package:dio/dio.dart';
+import 'package:dio_cache_interceptor/dio_cache_interceptor.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'custom_dio_cache_interceptor.g.dart';
+
+@Riverpod(keepAlive: true)
+DioCacheInterceptor dioCacheInterceptor(Ref ref) {
+  return DioCacheInterceptor(
+    options: CacheOptions(
+      store: MemCacheStore(),
+      policy: CachePolicy.refreshForceCache,
+      maxStale: const Duration(minutes: 10),
+      allowPostMethod: true,
+    ),
+  );
+}
+
+class CustomDioCacheInterceptor extends Interceptor {
+  CustomDioCacheInterceptor(this.ref);
+
+  final Ref ref;
+
+  DioCacheInterceptor get _interceptor => ref.read(dioCacheInterceptorProvider);
+
+  @override
+  void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
+    _interceptor.onRequest(options, handler);
+  }
+
+  @override
+  void onResponse(Response response, ResponseInterceptorHandler handler) {
+    _interceptor.onResponse(response, handler);
+  }
+
+  @override
+  void onError(DioException err, ErrorInterceptorHandler handler) {
+    _interceptor.onError(err, handler);
+  }
+}
